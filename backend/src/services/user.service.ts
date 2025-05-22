ab718c0b13091bbe819e2e581adfb6b37c7a3bc4
schmeekygeek <ss6087982@gmail.com>
2025-05-22 14:29:34 +0530

    Hash password before saving

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,5 +1,8 @@
 import * as userRepository from '../repositories/user.repository';
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (name: string, email: string, password: string) => {
+  const hashedPassword = await bcrypt.hash(password, 10)
+  password = hashedPassword
   return await userRepository.createUser({ name, email, password });
 };
