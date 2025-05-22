c77bac58cb2f77e6cc34915195ebc49ba4c4a283
schmeekygeek <ss6087982@gmail.com>
2025-05-22 14:18:42 +0530

    Added email validation

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -6,6 +6,12 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: function (email: string) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      },
+      message: (props: any) => `${props.value} is not a valid email address`
+    },
   },
   password: { type: String, required: true },
 }, { timestamps: true }
