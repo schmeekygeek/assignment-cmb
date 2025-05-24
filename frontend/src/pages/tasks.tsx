089715e9c6acccf8405c4b1dde955e95832020f1
schmeekygeek <ss6087982@gmail.com>
2025-05-24 21:30:10 +0530

    frontend - add empty task message

--- frontend/src/pages/tasks.tsx
+++ frontend/src/pages/tasks.tsx
@@ -23,44 +23,44 @@ export const TaskPage = () => {
   useEffect(() => {
     getAllTasks();
   }, [])
-  
+
   return (
     <div className="items-center justify-center">
-      <div className="flex flex-row justify-around">
+      <div className="flex flex-row justify-around mb-4">
         <h1 className="text-2xl font-bold tracking-tight lg:text-2xl">
           Your Tasks
         </h1>
-        <DrawerDialogTaskForm refresh={getAllTasks}/>
+        <DrawerDialogTaskForm refresh={getAllTasks} />
       </div>
       <div className="flex items-center justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center">
-        { 
-          isLoading ? 
-              (
-                <>
-                  <LoadingSkeleton /> 
-                  <LoadingSkeleton /> 
-                  <LoadingSkeleton /> 
-                  <LoadingSkeleton /> 
-                  <LoadingSkeleton /> 
-                  <LoadingSkeleton /> 
-                </>
-              ) : (
-              tasks.map(task => {
-              return <TaskCard
-                  props={{
-                    _id: task._id,
-                    title: task.title,
-                    description: task.description,
-                    dueDate: task.dueDate,
-                    status: task.status,
-                    userId: task.userId,
-                  }}
-                  refresh={getAllTasks}
-                />
-            })
-          )
-        }
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-4">
+          {isLoading ? (
+            <>
+              <LoadingSkeleton />
+              <LoadingSkeleton />
+              <LoadingSkeleton />
+              <LoadingSkeleton />
+              <LoadingSkeleton />
+              <LoadingSkeleton />
+            </>
+          ) : tasks.length === 0 ? (
+            <h1 className="text-gray-500 text-center col-span-full lg:text-xl text-lg">You have no tasks currently</h1>
+          ) : (
+            tasks.map(task => (
+              <TaskCard
+                key={task._id}
+                props={{
+                  _id: task._id,
+                  title: task.title,
+                  description: task.description,
+                  dueDate: task.dueDate,
+                  status: task.status,
+                  userId: task.userId,
+                }}
+                refresh={getAllTasks}
+              />
+            ))
+          )}
         </div>
       </div>
     </div>
